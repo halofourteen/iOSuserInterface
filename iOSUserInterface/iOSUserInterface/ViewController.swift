986f1be011ab787113fcc7e13ef76a84138e7ca1
Yuriy Mayatnikov <ymayatnikov@gmail.com>
2020-07-03 23:20:44 +0300

    init

---
+++ iOSUserInterface/iOSUserInterface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSUserInterface
+//
+//  Created by Yuriy Mayatnikov on 03.07.2020.
+//  Copyright © 2020 Yuriy Mayatnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
